702b75189c53ff1762816edd4aa526855ba6f6f6
Benyamin Farzaneh <64587077+Benfarzaneh@users.noreply.github.com>
2022-08-13 11:35:29 +0430

    Create 1-5- Strings.py

---
+++ 1-5- Strings.py
@@ -0,0 +1,38 @@
+# What is the value of the variable a after the followingcode is executed?
+a = "1"
+print(a)
+
+# What is the value of the variable b after the followingcode is executed?
+b = "2"
+print(b)
+
+# What is the value of the variable b after the followingcode is executed?
+c = a+b
+print(c)
+
+# Consider the variable d using slicing to print out the first three elements:
+d = "ABCDEFG"
+print(d[:3])
+
+# Use a stride value of 2 to print out every srcond character of the string e:
+e = "clocrkrlelclt"
+print(e[::2])
+
+# Write a code to print out a backslash
+print("\\")
+
+# Convert the variable f to uppercase
+f = "You are wrong"
+U = f.upper()
+print(U)
+
+# Consider the variable g, and find the firstg index of the sub-string 'snow':
+g = "Mary has a little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+everywhere that Mary went The lamb was sure to go"
+I = g.find('snow')
+print(I)
+
+# In the variable g, replace the sub-string 'Mary' with 'Bob':
+b=g.replace('Mary','Bob')
+print(b)
