b6efebefb5dc1c69ecb5224c2486260ea3aa6dd7
Benyamin Farzaneh <64587077+Benfarzaneh@users.noreply.github.com>
2022-08-13 10:34:07 +0430

    Create 4- Expression and Variables in Python.py

---
+++ 4- Expression and Variables in Python.py
@@ -0,0 +1,11 @@
+# What is the Value of x where x = 3+2*2 ?
+x = 3+2*2
+print(x)
+
+# What is the Value of y where y = (3+2)*2 ?
+y = (3+2)*2
+print(y)
+
+# What is the Value of z where z = x+y ?
+z = x+y
+print(z)
